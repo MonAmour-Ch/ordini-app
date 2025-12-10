2d748c0d6bf863d4e3c11f279dea685affe14bb9
burba1996-wq <burba1996@gmail.com>
2025-12-10 15:31:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ import {
 
 /* ----------------- CONFIG FIREBASE (SOSTITUISCI QUI) ----------------- */
 const firebaseConfig = {
-  apiKey: "TUO_apiKey",
-  authDomain: "TUO_authDomain",
-  projectId: "TUO_projectId",
-  // altri campi...
+  apiKey: "AIzaSyC0SFan3-K074DG5moeqmu4mUgXtxCmTbg",
+  authDomain: "menu-6630f.firebaseapp.com",
+  projectId: "menu-6630f",
+  storageBucket: "menu-6630f.firebasestorage.app",
+  messagingSenderId: "250958312970",
+  appId: "1:250958312970:web:9a7929c07e8c4fa352d1f3",
+  measurementId: "G-GTQS2S4GNF"
 };
 /* --------------------------------------------------------------------- */
 
@@ -177,3 +180,4 @@ function initStaffPage(){
     });
   });
 }
+
